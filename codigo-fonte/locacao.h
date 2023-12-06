6aa6335f1c6e7269d8c1c8f5d95ceedd52ef3678
santwsvi <victorgsantosrocha@gmail.com>
2023-12-06 20:39:23 -0300

    Adicao do modulo de cadastro de locação - Close #6

---
+++ codigo-fonte/locacao.h
@@ -0,0 +1,15 @@
+// locacao.h
+
+#ifndef LOCACAO_H
+#define LOCACAO_H
+
+#include "estruturas.h"
+int locacaoExiste(Locacao locacoes[], int totalLocacoes, int codigo);
+int calcularDiferencaDias(const char *data_inicial, const char *data_final);
+void cadastrarLocacao(Locacao locacoes[], int *totalLocacoes, Cliente clientes[], int totalClientes, Veiculo veiculos[], int totalVeiculos);
+void exibirLocacoes(const Locacao locacoes[], int totalLocacoes, const Cliente clientes[], int totalClientes, const Veiculo veiculos[], int totalVeiculos);
+void carregarLocacoes(Locacao locacoes[], int *totalLocacoes);
+Veiculo *encontrarVeiculo(Veiculo veiculos[], int totalVeiculos, int codigo);
+
+
+#endif // LOCACAO_H
